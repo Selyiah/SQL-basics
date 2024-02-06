9e2631564333148e51ba15e22818c5206c1d086b
Sely <140277594+Selyiah@users.noreply.github.com>
2024-02-06 21:39:05 +0000

    Create Database-Cluedo.sql

---
+++ Database-Cluedo.sql
@@ -0,0 +1,26 @@
+-- Create our databse 
+CREATE DATABASE Cluedo; 
+
+-- Use the correct database 
+USE Cluedo; 
+
+-- Create our suspects table 
+CREATE TABLE Suspects (
+    FullName varchar(50) NOT NULL, 
+    Salutation varchar(50), 
+    Gender varchar(1),
+    Age int,
+    TokenColour varchar(10)
+);
+
+-- Insert our characters into our table 
+INSERT INTO Suspects 
+VALUES 
+("Scarlett", "Miss", "F", 42, "Red"),
+("Green", "Reverend", "M", 50, "Green"), 
+("Mustard", "Colonel", "M", 65, "Yellow"),
+("Plum", "Professor", "M", 37, "Purple"), 
+("Orchid", "Doctor", "F", 32, "Pink"), 
+("Peacock", "Mrs", "F", 35, "Blue"); 
+
+SELECT * FROM Suspects; 
